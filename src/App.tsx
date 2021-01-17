e2182a33cf99f64976aab904067f8e697a7621ed
Cyril Mzyr <cyril.mazouyer@pm.me>
2021-01-17 23:30:46 +0100

    build: update React to v11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-
-import "./App.scss";
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Vélo-cité</h1>
-      <Switch>{/*<Route path="/" exact component={undefined} />*/}</Switch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
