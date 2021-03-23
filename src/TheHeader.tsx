0725b9c75cc8a647ad3c9003cce8a5ac9f91d711
Cyril Mzyr <cyril.mazouyer@pm.me>
2021-03-23 22:18:32 +0100

    style(the header): add logo and use Tailwind

--- src/TheHeader.tsx
+++ src/TheHeader.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './TheHeader.scss';
+import logo from './logo.png';
 
 function TheHeader() {
   return (
-    <header className="TheHeader">
-        <img src="{logo}" className="TheHeader-logo" alt="Logo Vélo-Cité"/>
-        <h1>Cyclo-fiches par Vélo-Cité</h1>
-        <span></span>
+    <header className="TheHeader relative bg-gray-300 p-5">
+      <img src={logo} className="TheHeader-logo" alt="Logo Vélo-Cité"/>
+      <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl text-white">Cyclo-fiches par Vélo-Cité</h1>
     </header>
   );
 }
